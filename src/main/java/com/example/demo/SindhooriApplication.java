7fccb59b3cd7a4a96ec87c59c7f74ede29bb3a91
Sindhoori Kaza <84836309+sindhoori-kaza@users.noreply.github.com>
2023-01-16 11:06:26 +0530

    Update SindhooriApplication.java

--- src/main/java/com/example/demo/SindhooriApplication.java
+++ src/main/java/com/example/demo/SindhooriApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RithikaApplication {
+public class SindhooriApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SindhooriApplication.class, args);
